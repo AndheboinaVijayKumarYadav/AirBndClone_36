9de190c40b6441b95613953ce1afea0c38e184aa
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2023-10-17 12:43:45 +0530

    structure of react app with Nav.js

---
+++ airbnd/src/App.js
@@ -0,0 +1,13 @@
+import Nav from './Components/Nav';
+import logo from './Images/airbnb1.png'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+         <Nav logo={logo}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ airbnd/src/Components/Nav.js
@@ -0,0 +1,8 @@
+
+export default function Nav({logo}) {
+    return (
+        <nav>
+            <img src ={logo} alt="Airbnb logo" className="nav--logo"></img>
+        </nav>
+    )
+}
